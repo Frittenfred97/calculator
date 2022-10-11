55957cf7eaa71dc0836c8d71d57d009dad8f67f8
Frederic Carboneras <carboneras.frederic@gmail.com>
2022-10-11 15:59:18 +0200

    Added Calculator functionality

--- script.js
+++ script.js
@@ -1,41 +1,144 @@
+//declaration of global variables used to displate the display
 let firstNumber = "";
 let secondNumber = "";
-let operator = ""; 
+let operator = "";
+
+//declaration of all connections between JS and HTML | Buttons & Display
 let numberButtons = document.querySelectorAll(".nmb-btn");
-let number = 0;
+let operatorButton = document.querySelectorAll(".operator");
+let deleteButton = document.querySelector(".delete");
+let clearButton = document.querySelector(".clear");
+let equalButton = document.querySelector(".equal");
+let dotButton = document.querySelector(".button-15");
+
+//Loop through every numeral Button to store the Value and display the Input.
+numberButtons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    switch (operator) {
+      case "+":
+      case "-":
+      case "*":
+      case "/":
+        if (e.target.value == ".") {
+          e.target.disabled = true;
+          if (secondNumber == "") {
+            secondNumber = "0";
+          }
+        }
+        secondNumber += btn.value;
+        break;
+      default:
+        if (e.target.value == ".") {
+          e.target.disabled = true;
+          if (firstNumber == "") {
+            firstNumber = "0";
+          }
+        }
+        firstNumber += btn.value;
+        break;
+    }
+
+    let showDisplay = firstNumber + operator + secondNumber;
+    document.getElementById("nmb").innerHTML = showDisplay;
+  });
+});
+
+//Clear button to reset everything
+clearButton.addEventListener("click", () => {
+  dotButton.disabled = false;
+  firstNumber = "";
+  operator = "";
+  secondNumber = "";
+
+  let showDisplay = firstNumber + operator + secondNumber;
+  document.getElementById("nmb").innerHTML = showDisplay;
+});
+
+//Operator button to define the operator button
+operatorButton.forEach((op) => {
+  op.addEventListener("click", () => {
+    dotButton.disabled = false;
+    operator = op.value;
+
+    let showDisplay = firstNumber + operator + secondNumber;
+    document.getElementById("nmb").innerHTML = showDisplay;
+  });
+});
+
+//Delete button to delete the last set charakter
+deleteButton.addEventListener("click", function () {
+  switch (operator) {
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+      secondNumber = secondNumber.substring(0, secondNumber.length - 1);
+      if (secondNumber.includes != ".") {
+        dotButton.disabled = false;
+      }
+      break;
+    default:
+      firstNumber = firstNumber.substring(0, firstNumber.length - 1);
+      if (secondNumber.includes != ".") {
+        dotButton.disabled = false;
+      }
+      break;
+  }
+
+  let showDisplay = firstNumber + operator + secondNumber;
+  document.getElementById("nmb").innerHTML = showDisplay;
+});
+
+equalButton.addEventListener("click", () => {
+    operate();
+} 
+
+);
 
 function add(a, b) {
-    return  a + b;
+  result = a + b;
+  operator = "";
+  secondNumber = "";
+  firstNumber = Math.round(result);
 }
 
 function subtract(a, b) {
-    return a - b;
+  result = a - b;
+  operator = "";
+  secondNumber = "";
+  Math.round(firstNumber);
 }
 
 function multiply(a, b) {
-    return a * b;
+  firstNumber = a * b;
+  operator = "";
+  secondNumber = "";
+  Math.round(firstNumber);
 }
 
 function divide(a, b) {
-    return a / b;
+  firstNumber = a / b;
+  operator = "";
+  secondNumber = "";
+  Math.round(firstNumber);
 }
 
-function operate(a, b) {
+function operate () {
+    dotButton.disabled = false;
 
+    a = parseFloat(firstNumber);
+    b = parseFloat(secondNumber);
+  
     if (operator == "+") {
-        return add(a, b);
+      add(a, b);
     } else if (operator == "-") {
-        return subtract(a, b); 
-        } else if (operator == "*") {
-            return multiply(a, b);
-        } else {
-            return divide(a, b);
-        }
+      subtract(a, b);
+    } else if (operator == "*") {
+      multiply(a, b);
+    } else {
+      divide(a, b);
+    }
+  
+    let showDisplay = firstNumber + operator + secondNumber;
+    document.getElementById("nmb").innerHTML = showDisplay;
 }
-
-numberButtons.forEach((btn) => {
-    btn.addEventListener("click", () => {
-         number = btn.value;
-         document.getElementById("nmb").innerHTML = number;
-    });
-});
